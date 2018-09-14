519a7b5ee1cb020a958d0028fe3a917273e72eda
Sam <lemonverse@gmail.com>
2018-09-15 10:14:59 +1200

    Add registry name to ritual tooltip

--- src/main/java/party/lemons/arcaneworld/compat/jei/RitualRecipeWrapper.java
+++ src/main/java/party/lemons/arcaneworld/compat/jei/RitualRecipeWrapper.java
@@ -6,8 +6,13 @@ import mezz.jei.api.recipe.IRecipeWrapper;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.TextFormatting;
 import party.lemons.arcaneworld.crafting.ritual.impl.Ritual;
 import party.lemons.arcaneworld.crafting.ritual.impl.RitualCreateItem;
+import sun.net.www.MimeEntry;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -29,6 +34,22 @@ public class RitualRecipeWrapper implements IRecipeWrapper
     {
         return ritual;
     }
+
+    public List<String> getTooltipStrings(int mouseX, int mouseY)
+    {
+        String str = I18n.format(ritual.getTranslationKey());
+        int length = Minecraft.getMinecraft().fontRenderer.getStringWidth(str);
+        int height = Minecraft.getMinecraft().fontRenderer.FONT_HEIGHT;
+        ArrayList<String> list = new ArrayList<>();
+        if(mouseX >= 2 && mouseX <= 2 + length)
+        {
+            if(mouseY >= -2 && mouseY <= -2 + height)
+                list.add(TextFormatting.DARK_GRAY + ritual.getRegistryName().toString());
+        }
+
+        return list;
+    }
+
 
     @Override
     public void drawInfo(Minecraft minecraft, int recipeWidth, int recipeHeight, int mouseX, int mouseY)
