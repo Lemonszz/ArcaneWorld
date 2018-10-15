35b4a4af0914a767bceb6aac5a08bd289e88c6cd
Sam <lemonverse@gmail.com>
2018-10-15 13:18:43 +1300

    Bandaid fix on falling out of dungeons

--- src/main/java/party/lemons/arcaneworld/gen/dungeon/dimension/TeleporterDungeon.java
+++ src/main/java/party/lemons/arcaneworld/gen/dungeon/dimension/TeleporterDungeon.java
@@ -1,6 +1,7 @@
 package party.lemons.arcaneworld.gen.dungeon.dimension;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.Teleporter;
 import net.minecraft.world.World;
@@ -28,11 +29,13 @@ public class TeleporterDungeon extends Teleporter
 
         if(!hasGenerated)
         {
+
             DungeonSavedData data = DungeonSavedData.getInstance(world);
             int offset = data.getDungeonCount() * 250;
             data.addDungeon();
 
             offsetPos = new BlockPos(offset, 40, 0);
+            world.setBlockState(new BlockPos(offsetPos.getX() + (DungeonGenerator.ROOM_WIDTH / 2), offsetPos.up(3).getY(), offsetPos.getZ() + (DungeonGenerator.ROOM_WIDTH / 2)), Blocks.BEDROCK.getDefaultState());
 
             DungeonGenerator generator = new DungeonGenerator(world, offsetPos);
             generator.generateRoom(0, 0);
@@ -41,7 +44,7 @@ public class TeleporterDungeon extends Teleporter
             hasGenerated = true;
         }
 
-        entity.setLocationAndAngles(offsetPos.getX() + (DungeonGenerator.ROOM_WIDTH / 2), offsetPos.up(2).getY(), offsetPos.getZ() + (DungeonGenerator.ROOM_WIDTH / 2), entity.rotationYaw, 0.0F);
+        entity.setLocationAndAngles(offsetPos.getX() + (DungeonGenerator.ROOM_WIDTH / 2), offsetPos.up(3).getY(), offsetPos.getZ() + (DungeonGenerator.ROOM_WIDTH / 2), entity.rotationYaw, 0.0F);
         entity.motionX = 0.0D;
         entity.motionY = 0.0D;
         entity.motionZ = 0.0D;
--- src/main/java/party/lemons/arcaneworld/handler/ArcaneWorldGeneralEventHandler.java
+++ src/main/java/party/lemons/arcaneworld/handler/ArcaneWorldGeneralEventHandler.java
@@ -4,12 +4,18 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.monster.AbstractIllager;
 import net.minecraft.entity.monster.EntityEvoker;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Items;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.WorldServer;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 import party.lemons.arcaneworld.ArcaneWorld;
 import party.lemons.arcaneworld.config.ArcaneWorldConfig;
+import party.lemons.arcaneworld.gen.dungeon.dimension.DungeonDimension;
+import party.lemons.arcaneworld.gen.dungeon.dimension.TeleporterDungeonReturn;
 import party.lemons.arcaneworld.util.ArcaneWorldUtil;
 
 /**
@@ -36,4 +42,23 @@ public final class ArcaneWorldGeneralEventHandler {
             }
         }
     }
+
+    @SubscribeEvent
+    public static void onEntityTick(TickEvent.PlayerTickEvent event)
+    {
+        if(event.player.world.provider.getDimension() == DungeonDimension.TYPE.getId())
+        {
+            if(event.player.posY < 10)
+            {
+                event.player.fallDistance = 0;
+                event.player.sendStatusMessage(new TextComponentString("You experienced a bug in Arcane World, please report this on the Curse page!"), false);
+
+                if(!event.player.world.isRemote)
+                {
+                    event.player.changeDimension(0, new TeleporterDungeonReturn((WorldServer) event.player.world));
+                }
+            }
+        }
+    }
+
 }
